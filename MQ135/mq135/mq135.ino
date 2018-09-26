b909a2c16bc3634a9cfaba1a51db6ecbd8ed17b8
mauro rivera <julianriveraavalo@gmail.com>
2018-09-26 12:35:12 -0500

    calibrar mq135

---
+++ MQ135/mq135/mq135.ino
@@ -0,0 +1,16 @@
+int pinsensormq135 = 0;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  pinsensormq135 = analogRead(0); 
+  float mq135_voltaje = pinsensormq135 * (5.0 / 1023.0);
+  float mq135_resistencia = 1000*((5-mq135_voltaje)/mq135_voltaje);
+  double amoniaco = 161.7*pow(mq135_resistencia/5463, -2.26);
+        
+  Serial.println(pinsensormq135, DEC);
+  Serial.println(amoniaco);
+  delay(250); 
+
+}
