6a59cd97e09328838c9216593603fcf1d17b70b3
mauro rivera <julianriveraavalo@gmail.com>
2018-10-26 23:51:23 -0500

    envio de mensaje y configuracion de conecciones
    Se establecieron la condiciones para envio de mensaje

--- dosmetodos1/dosmetodos1.ino
+++ dosmetodos1/dosmetodos1.ino
@@ -3,9 +3,11 @@ SoftwareSerial SIM900(7, 8);
 SoftwareSerial wifi(3, 2);
 #define DEBUG true
 boolean decision = false;
-String temperatura = "32";
-String humedad = "56";
-String amoniaco = "12";
+String temperatura;
+String humedad ;
+String amoniaco;
+
+
 
 void setup() {
   Serial.begin(9600);  //Configura velocidad del puerto serie del Arduino
@@ -13,12 +15,16 @@ void setup() {
   wifi.begin(9600);
 
 
+
+
   //Configura velocidad del puerto serie para el SIM900
 }
 
 void loop() {
+  temperatura = "37";
+  amoniaco = "10";
+  humedad = "65";
   Serial.println(decision);
-  // enviarmsm();
   enviarwifi();
   if (decision == false) {
     enviarsim();
@@ -26,19 +32,28 @@ void loop() {
   else {
     decision = false;
   }
+
+  if (temperatura.toInt() > 35 || amoniaco.toInt() > 10 || humedad.toInt() > 60) {
+    enviarmsm();
+  }
+
+
   Serial.println("2: " + decision);
 }
 
 
 void enviarsim() {
+  String peticion = "GET http://tepremiapp.com/sensor/datos.php?temperatura=" + temperatura + "&humedad=" + humedad + "&amoniaco=" + amoniaco + "\r\nHost:www.tepremiapp.com";
+  int longitud = peticion.length();
   sendDatasim("AT\r\n", 5000, DEBUG);
   sendDatasim("AT+CGATT=1\r\n", 5000, DEBUG);
   sendDatasim("AT+CSTT=\"internet.comcel.com.co\",\"comcel\",\"comcel\"\r\n", 5000, DEBUG);
   sendDatasim("AT+CIICR\r\n", 5000, DEBUG);
   sendDatasim("AT+CIFSR\r\n", 5000, DEBUG);
   sendDatasim("AT+CIPSTART=\"TCP\",\"www.tepremiapp.com\",\"80\"\r\n", 5000, DEBUG);
-  sendDatasim("AT+CIPSEND=101\r\n", 5000, DEBUG);
-  String respuesta = sendDatasim("GET http://tepremiapp.com/sensor/datos.php?temperatura=1&humedad=2&amoniaco=3\r\nHost:www.tepremiapp.com", 10000, DEBUG);
+  sendDatasim("AT+CIPSEND=" + String(longitud) + "\r\n", 5000, DEBUG);
+  //String respuesta = sendDatasim("GET http://tepremiapp.com/sensor/datos.php?temperatura=" + temperatura + "&humedad=" + humedad + "&amoniaco=" + amoniaco + "\r\nHost:www.tepremiapp.com", 10000, DEBUG);
+  String respuesta = sendDatasim(peticion, 10000, DEBUG);
   int dato = respuesta.indexOf("true");
   if (dato > 0) {
     Serial.println("Se almacenaron datos por sim");
@@ -67,11 +82,13 @@ String sendDatasim(String command, const int timeout, boolean debug) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 void enviarwifi() {
+  String peticion = "GET http://tepremiapp.com/sensor/datos.php?temperatura=" + temperatura + "&humedad=" + humedad + "&amoniaco=" + amoniaco + "\r\nHost:www.tepremiapp.com";
+  int longitud = peticion.length();
   sendDatawifi("AT+RST\r\n", 10000, DEBUG);
   sendDatawifi("AT+CWJAP=\"Movistar_22160435\",\"00929329818\"\r\n", 10000, DEBUG);
   sendDatawifi("AT+CIPSTART=\"TCP\",\"107.180.50.184\",80\r\n", 5000, DEBUG);
-  sendDatawifi("AT+CIPSEND=101\r\n", 1000, DEBUG);
-  String respuesta1 = sendDatawifi("GET http://tepremiapp.com/sensor/datos.php?temperatura=5&humedad=6&amoniaco=7\r\nHost:www.tepremiapp.com", 3000, DEBUG);
+  sendDatawifi("AT+CIPSEND=" + String(longitud) + "\r\n", 1000, DEBUG);
+  String respuesta1 = sendDatawifi(peticion, 3000, DEBUG);
   int dato1 = respuesta1.indexOf("true");
   if (dato1 > 0) {
     Serial.println("Se almacenaron datos por wifi");
@@ -104,7 +121,7 @@ String sendDatawifi(String command, const int timeout, boolean debug) {
 void enviarmsm()
 {
   sendDatasim("AT+CMGF=1\r\n", 1000, DEBUG);
-  sendDatasim("AT+CMGS=\"3104706624\"\r\n", 1000, DEBUG);
-  sendDatasim("SMS enviado desde un Arduino", 100, DEBUG);
+  sendDatasim("AT+CMGS=\"3218334774\"\r\n", 1000, DEBUG);
+  sendDatasim("ALERTA!!\nSr Mesa: \nSe recomienda tomar medidas en su granja\nTemperatura: " + temperatura + "C\nHumedad:" + humedad + "%\nAmoniaco:"+amoniaco+"ppm", 100, DEBUG);
   SIM900.println((char)26);
 }
